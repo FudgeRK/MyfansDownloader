e83026dd87bd467981a32e5a8a4dbb113f2905aa
O3O3O <chaiyapos72@gmail.com>
2023-11-25 06:07:02 +0700

    Add files via upload

---
+++ myfans_dl.py
@@ -0,0 +1,207 @@
+import os
+import datetime
+import requests
+import random
+import string
+import subprocess
+import concurrent.futures
+import configparser
+
+# Function to read headers from a file
+def read_headers_from_file(filename):
+    headers = {}
+    with open(filename, 'r') as file:
+        for line in file:
+            key, value = line.strip().split(': ')
+            headers[key.lower()] = value
+    return headers
+
+# Function to remove a line with a specified ID from the input file
+def remove_line_with_id(input_file_path, post_id):
+    with open(input_file_path, 'r') as file:
+        lines = file.readlines()
+    with open(input_file_path, 'w') as file:
+        for line in lines:
+            if line.strip() != post_id:
+                file.write(line)
+
+# Function to download and convert the .m3u8 playlist to .mp4 using FFmpeg
+def DL_File(session, m3u8_url, output_file):
+    try:
+        # Check if the output file already exists
+        if os.path.isfile(output_file):
+            # If it exists, rename the file by adding a timestamp to its name
+            timestamp = datetime.datetime.now().strftime("%H%M%S")
+            base_name, extension = os.path.splitext(output_file)
+            new_output_file = f"{base_name}_{timestamp}{extension}"
+            os.rename(output_file, new_output_file)
+            print(f"Renamed existing file to {new_output_file}")
+            print('----------------------------------------------------------------')
+
+        # Use FFmpeg to download and convert the .m3u8 playlist to .mp4
+        subprocess.run(["ffmpeg", "-i", m3u8_url, "-c:v", "copy", "-c:a", "copy", "-loglevel", "warning", "-stats", output_file], check=True)
+        print('----------------------------------------------------------------')
+        print(f"Downloaded and converted to {output_file}")
+        print('----------------------------------------------------------------')
+        return True
+    except subprocess.CalledProcessError as e:
+        print(f"Error while downloading and converting .m3u8 to .mp4: {e}")
+        return False
+
+# Create a session with custom headers
+session = requests.Session()
+
+# Check if the configuration file exists
+config_file_path = 'config.ini'
+
+if os.path.isfile(config_file_path):
+    # If the config file exists, read the output directory from it
+    config = configparser.ConfigParser()
+    config.read(config_file_path)
+    output_dir = config.get('Settings', 'output_dir')
+else:
+    # If the config file doesn't exist, prompt the user for the output directory
+    output_dir = input("Enter the output directory: ")
+
+    # Create the config file and save the output directory
+    config = configparser.ConfigParser()
+    config['Settings'] = {'output_dir': output_dir}
+    with open(config_file_path, 'w') as configfile:
+        config.write(configfile)
+
+# User input to select post IDs from a file or enter a single ID
+input_option = input("Choose an option ('file' or 'id'): ").strip().lower()
+
+if input_option == 'file':
+    # Read post IDs from a file
+    input_file_path = input("Enter the path to the file containing post IDs: ")
+    with open(input_file_path, 'r') as id_file:
+        post_ids = [line.strip() for line in id_file.readlines()]
+
+    selected_resolution = 'fhd'
+    original_resolution = selected_resolution
+
+    # Create a thread pool for concurrent downloading and conversion
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        # Process each post ID concurrently
+        for input_post_id in post_ids:
+            url = f"https://api.myfans.jp/api/v2/posts/{input_post_id}"
+            headers = read_headers_from_file("header.txt")
+            response = session.get(url, headers=headers)
+
+            if response.status_code == 200:
+                data = response.json()
+                main_videos = data['videos']['main']
+                name_creator = data['user']['username']
+
+                if main_videos:
+                    # Check if the selected resolution is available
+                    video_url = None
+                    for video in main_videos:
+                        if video["resolution"] == selected_resolution:
+                            video_url = video["url"]
+                            break
+
+                    if not video_url:
+                        # If the selected resolution is not available, switch to 480p
+                        if selected_resolution == "fhd":
+                            selected_resolution = "sd"
+                            print(f"not found the FHD file, switching to {selected_resolution} resolution.")
+                        elif selected_resolution == "sd":
+                            selected_resolution = "fhd"
+                            print(f"not found the file, switching to {selected_resolution} resolution.")
+
+                        # Try to find the new resolution
+                        for video in main_videos:
+                            if video["resolution"] == selected_resolution:
+                                video_url = video["url"]
+                                break
+
+                    if video_url:
+                        # Modify the URL based on the selected resolution
+                        video_base_url, video_extension = os.path.splitext(video_url)
+                        if selected_resolution == "fhd":
+                            target_resolution = "1080p"
+                        elif selected_resolution == "sd":
+                            target_resolution = "480p"
+
+                        # Add /{target_resolution}.m3u8 to the video URL
+                        m3u8_url = f"{video_base_url}/{target_resolution}.m3u8"
+
+                        # Download and convert the .m3u8 playlist to .mp4 concurrently
+                        characters = string.ascii_lowercase + string.digits
+                        random_string = ''.join(random.choice(characters) for _ in range(6))
+                        mp4_output_file = os.path.join(output_dir, f"{name_creator}_video_{random_string}.mp4")
+                        future = executor.submit(DL_File, session, m3u8_url, mp4_output_file)
+                        future.add_done_callback(lambda x: remove_line_with_id(input_file_path, input_post_id) if x.result() else None)
+                    else:
+                        print(f"No video URL found for resolution '{selected_resolution}' in post ID {input_post_id}.")
+                else:
+                    print(f"No videos found or You don't have access this file")
+            else:
+                print(f"Request failed for post ID {input_post_id} with status code:", response.status_code)
+                  
+else:
+
+    input_post_id = input("Enter the post ID: ")
+    post_ids = [input_post_id]
+    selected_resolution = 'fhd'
+    original_resolution = selected_resolution
+
+    for input_post_id in post_ids:
+        url = f"https://api.myfans.jp/api/v2/posts/{input_post_id}"
+        headers = read_headers_from_file("header.txt")
+        response = session.get(url, headers=headers)
+
+        if response.status_code == 200:
+            data = response.json()
+            main_videos = data['videos']['main']
+            name_creator = data['user']['username']
+
+            if main_videos:
+                # Check if the selected resolution is available
+                video_url = None
+                for video in main_videos:
+                    if video["resolution"] == selected_resolution:
+                        video_url = video["url"]
+                        break
+
+                if not video_url:
+                    # If the selected resolution is not available, switch to 480p
+                    if selected_resolution == "fhd":
+                        selected_resolution = "sd"
+                        print(f"not found the FHD file, switching to {selected_resolution} resolution.")
+                    elif selected_resolution == "sd":
+                        selected_resolution = "fhd"
+                        print(f"not found the file, switching to {selected_resolution} resolution.")
+
+                    # Try to find the new resolution
+                    for video in main_videos:
+                        if video["resolution"] == selected_resolution:
+                            video_url = video["url"]
+                            break
+
+                if video_url:
+                    # Modify the URL based on the selected resolution
+                    video_base_url, video_extension = os.path.splitext(video_url)
+                    if selected_resolution == "fhd":
+                        target_resolution = "1080p"
+                    elif selected_resolution == "sd":
+                        target_resolution = "480p"
+
+                    # Add /{target_resolution}.m3u8 to the video URL
+                    m3u8_url = f"{video_base_url}/{target_resolution}.m3u8"
+
+                    # Download and convert the .m3u8 playlist to .mp4
+                    if video_url:
+                        characters = string.ascii_lowercase + string.digits
+                        random_string = ''.join(random.choice(characters) for _ in range(6))
+                        mp4_output_file = os.path.join(output_dir, f"{name_creator}_video_{random_string}.mp4")
+                        if DL_File(session, m3u8_url, mp4_output_file):
+                            pass
+                    else:
+                        print(f"No video URL found for resolution '{selected_resolution}'.")
+            else:
+                print(f"No '{selected_resolution}' videos found in the API response for post ID {input_post_id}.")
+        else:
+            print(f"Request failed for post ID {input_post_id} with status code:", response.status_code)
---
+++ myfans_scrap.py
@@ -0,0 +1,173 @@
+import requests
+import json
+import os
+import configparser
+
+def read_headers_from_file(filename):
+    headers = {}
+    with open(filename, 'r') as file:
+        for line in file:
+            key, value = line.strip().split(': ')
+            headers[key.lower()] = value
+    return headers
+
+# Check if the configuration file exists
+config_file_path = 'config.ini'
+
+if os.path.isfile(config_file_path):
+    # If the config file exists, read the output directory from it
+    config = configparser.ConfigParser()
+    config.read(config_file_path)
+    output_dir = config.get('Settings', 'output_dir')
+else:
+    # If the config file doesn't exist, prompt the user for the output directory
+    output_dir = input("Enter the output directory: ")
+
+    # Create the config file and save the output directory
+    config = configparser.ConfigParser()
+    config['Settings'] = {'output_dir': output_dir}
+    with open(config_file_path, 'w') as configfile:
+        config.write(configfile)
+
+# Prompt the user to enter a new username
+name_creator = input("Enter a name creator (no require @): ")
+
+# Update the base URL with the new username
+new_base_url = f"https://api.myfans.jp/api/v2/users/show_by_username?username={name_creator}"
+
+headers = read_headers_from_file("header.txt")
+
+# Retrieve the "id" from the new API endpoint
+response = requests.get(new_base_url, headers=headers)
+new_json_data = response.json()
+user_id = new_json_data.get("id")
+
+# Define the initial page and base URL
+page = 0
+if user_id:
+    base_url = f"https://api.myfans.jp/api/v2/users/{user_id}/posts?sort_key=publish_start_at&page="
+else:
+    print("Failed to retrieve user id from the new API endpoint.")
+    exit()  # Exit the script if user_id is not available
+
+# Prompt the user to choose what type of posts to display
+print("Choose the type of posts to display:")
+print("1. Video")
+print("2. Image")
+print("3. Show All")
+choice = input("Enter your choice (1/2/3): ")
+
+# Define dictionaries to store counts for video and image types
+video_count = 0
+image_count = 0
+video_posts = []
+image_posts = []
+all_posts = []
+
+while True:
+    url = base_url + str(page)
+    response = requests.get(url, headers=headers)
+    json_data = response.json()
+
+    # Check if "pagination" "next" is null
+    if json_data.get("pagination", {}).get("next") is None:
+        break  # Exit the loop if "next" is null
+
+    # Append the posts from the current page to the respective lists
+    posts = json_data.get("data", [])
+    for post in posts:
+        post_kind = post.get("kind")
+        if post_kind == "video":
+            video_posts.append(post)
+        elif post_kind == "image":
+            image_posts.append(post)
+        all_posts.append(post)
+
+    # Move to the next page
+    page += 1
+
+    # Iterate through the posts in the JSON data
+    for post in json_data.get("data", []):
+        post_id = post.get("id")
+        post_kind = post.get("kind")
+        humanized_publish_start_at = post.get("humanized_publish_start_at")
+        is_free = post.get("free")
+
+        if post_id and post_kind and humanized_publish_start_at:
+            # Check if the post is a video or an image
+            if choice == "1" and post_kind == "video":
+                video_count += 1
+                print(f"ID: {post_id}, Publish Date: {humanized_publish_start_at}, Free: {'yes' if is_free else 'no'}")
+            elif choice == "2" and post_kind == "image":
+                image_count += 1
+                print(f"ID: {post_id}, Publish Date: {humanized_publish_start_at}, Free: {'yes' if is_free else 'no'}")
+            elif choice == "3":
+                # Show all posts
+                print(f"ID: {post_id}, Type: {post_kind}, Publish Date: {humanized_publish_start_at}, Free: {'yes' if is_free else 'no'}")
+                if post_kind == "video":
+                    video_count += 1
+                elif post_kind == "image":
+                    image_count += 1
+
+# Print the total counts for video and image types
+if choice == "1":
+    print(f"Total Video Count: {video_count}")
+if choice == "2":
+    print(f"Total Image Count: {image_count}")
+if choice == "3":
+    print(f"Total Video Count: {video_count}")
+    print(f"Total Image Count: {image_count}")
+
+# Export data
+export_id_choice = input("Do you want to export 'ID'? (Yes|y/No|n): ").strip().lower()
+if export_id_choice == "yes" or export_id_choice == "y":
+
+    if choice == "1" and video_posts:
+        filename = os.path.join(output_dir, f"{name_creator}_video_id.txt")
+        if os.path.isfile(filename):
+            base, ext = os.path.splitext(filename)
+            count = 1
+            while os.path.isfile(filename):
+                filename = os.path.join(output_dir, f"{base}_{count}{ext}")
+                count += 1
+        with open(filename, "w") as video_posts_id_file:
+            for post in video_posts:
+                post_id = post.get("id")
+                video_posts_id_file.write(f"{post_id}\n")
+        print(f"Video data exported to {filename}")
+    else:
+        exit()
+
+    if choice == "2" and image_posts:
+        filename = os.path.join(output_dir, f"{name_creator}_image_id.txt")
+        if os.path.isfile(filename):
+            base, ext = os.path.splitext(filename)
+            count = 1
+            while os.path.isfile(filename):
+                filename = os.path.join(output_dir, f"{base}_{count}{ext}")
+                count += 1
+        with open(filename, "w") as image_posts_id_file:
+            for post in image_posts:
+                post_id = post.get("id")
+                image_posts_id_file.write(f"{post_id}\n")
+        print(f"Image data exported to {filename}")
+    else:
+        exit()
+
+    if choice == "3" and all_posts:
+        filename = os.path.join(output_dir, f"{name_creator}_all_id.txt")
+        if os.path.isfile(filename):
+            base, ext = os.path.splitext(filename)
+            count = 1
+            while os.path.isfile(filename):
+                filename = os.path.join(output_dir, f"{base}_{count}{ext}")
+                count += 1
+        with open(filename, "w") as all_posts_id_file:
+            for post in all_posts:
+                post_id = post.get("id")
+                all_posts_id_file.write(f"{post_id}\n")
+        print(f"All data exported to {filename}")
+    else:
+        exit()
+else:
+    exit()
