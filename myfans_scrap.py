1dec93bc9770bd99bc437d6b0205d5d898dfe3e7
FudgeRookie <chaiyapos72@gmail.com>
2024-02-24 06:18:32 +0700

    Update myfans_scrap.py
    Clean

--- myfans_scrap.py
+++ myfans_scrap.py
@@ -3,7 +3,6 @@ import json
 import os
 import configparser
 
-# Function to read headers from a file and store them in a dictionary
 def read_headers_from_file(filename):
     headers = {}
     with open(filename, 'r') as file:
@@ -12,60 +11,45 @@ def read_headers_from_file(filename):
             headers[key.lower()] = value
     return headers
 
-# Function to get posts for a specific page
 def get_posts_for_page(base_url, page, headers):
     url = base_url + str(page)
     response = requests.get(url, headers=headers)
     json_data = response.json()
     return json_data.get("data", [])
 
-# Check if the configuration file exists
 config_file_path = 'config.ini'
 
 if os.path.isfile(config_file_path):
-    # If the config file exists, read the output directory from it
     config = configparser.ConfigParser()
     config.read(config_file_path)
     output_dir = config.get('Settings', 'output_dir')
 else:
-    # If the config file doesn't exist, prompt the user for the output directory
     output_dir = input("Enter the output directory: ")
-
-    # Create the config file and save the output directory
     config = configparser.ConfigParser()
     config['Settings'] = {'output_dir': output_dir}
     with open(config_file_path, 'w') as configfile:
         config.write(configfile)
 
-# Prompt the user to enter a new username
 name_creator = input("Enter a name creator (no require @): ")
-
-# Update the base URL with the new username
 new_base_url = f"https://api.myfans.jp/api/v2/users/show_by_username?username={name_creator}"
-
 headers = read_headers_from_file("header.txt")
-
-# Retrieve the "id" from the new API endpoint
 response = requests.get(new_base_url, headers=headers)
 new_json_data = response.json()
 user_id = new_json_data.get("id")
 
-# Define the initial page and base URL
 page = 1
 if user_id:
     base_url = f"https://api.myfans.jp/api/v2/users/{user_id}/posts?sort_key=publish_start_at&page="
 else:
     print("Failed to retrieve user id from the new API endpoint.")
-    exit()  # Exit the script if user_id is not available
+    exit()
 
-# Prompt the user to choose what type of posts to display
 print("Choose the type of posts to display:")
 print("1. Video")
 print("2. Image")
 print("3. Show All")
 choice = input("Enter your choice (1/2/3): ")
 
-# Define dictionaries to store counts for video and image types
 video_count = 0
 image_count = 0
 video_posts = []
@@ -73,15 +57,12 @@ image_posts = []
 all_posts = []
 
 while True:
-    # Retrieve data for the current page
     page_data = get_posts_for_page(base_url, page, headers)
     
-    # Check if "pagination" "next" is null
     if not page_data:
-        break  # Exit the loop if there's no data on the page
+        break
     page += 1
 
-    # Append the posts from the current page to the respective lists
     for post in page_data:
         post_kind = post.get("kind")
         if post_kind == "video":
@@ -90,7 +71,6 @@ while True:
             image_posts.append(post)
         all_posts.append(post)
 
-    # Iterate through the posts in the JSON data for the current page
     for post in page_data:
         post_id = post.get("id")
         post_kind = post.get("kind")
@@ -98,7 +78,6 @@ while True:
         is_free = post.get("free")
 
         if post_id and post_kind and humanized_publish_start_at:
-            # Check if the post is a video or an image
             if choice == "1" and post_kind == "video":
                 video_count += 1
                 print(f"ID: {post_id}, Publish Date: {humanized_publish_start_at}, Free: {'yes' if is_free else 'no'}")
@@ -106,14 +85,12 @@ while True:
                 image_count += 1
                 print(f"ID: {post_id}, Publish Date: {humanized_publish_start_at}, Free: {'yes' if is_free else 'no'}")
             elif choice == "3":
-                # Show all posts
                 print(f"ID: {post_id}, Type: {post_kind}, Publish Date: {humanized_publish_start_at}, Free: {'yes' if is_free else 'no'}")
                 if post_kind == "video":
                     video_count += 1
                 elif post_kind == "image":
                     image_count += 1
 
-# Print the total counts for video and image types
 if choice == "1":
     print(f"Total Video Count: {video_count}")
 if choice == "2":
@@ -122,7 +99,6 @@ if choice == "3":
     print(f"Total Video Count: {video_count}")
     print(f"Total Image Count: {image_count}")
 
-# Export data
 export_id_choice = input("Do you want to export 'ID'? (Yes|y/No|n): ").strip().lower()
 if export_id_choice == "yes" or export_id_choice == "y":
     save_choice = input("Do you want to save only free posts? (Yes|y/No|n): ").strip().lower()
