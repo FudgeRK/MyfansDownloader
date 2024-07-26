3ac079f5c06646ffb4391147102730b3d3bac50d
FudgeRookie <chaiyapos72@gmail.com>
2024-07-26 20:23:49 +0700

    improve code
    - [scrap post] Reduce data duplication
    - Add back option
    - Added check if option not available is selected.

--- scripts/myfans_dl.py
+++ scripts/myfans_dl.py
@@ -13,16 +13,23 @@ def read_headers_from_file(filename):
             key, value = line.strip().split(': ')
             headers[key.lower()] = value
     return headers
-    
+
 def list_txt_files(directory):
     txt_files = [f for f in os.listdir(directory) if f.endswith('.txt')]
     return txt_files
 
 def choose_file_from_list(files):
-    for index, file in enumerate(files):
-        print(f"{index + 1}. {file}")
-    choice = int(input("Choose a file number: ")) - 1
-    return files[choice]
+    while True:
+        for index, file in enumerate(files):
+            print(f"{index + 1}. {file}")
+        choice = input("Choose a file number (or type '0' to go back): ")
+        if choice == '0':
+            return None
+        if choice.isdigit():
+            choice = int(choice) - 1
+            if 0 <= choice < len(files):
+                return files[choice]
+        print("Invalid choice. Please try again.")
 
 def DL_File(session, m3u8_url, output_file, input_post_id, output_folder, output_file_name):
     try:
@@ -55,171 +62,186 @@ def DL_File(session, m3u8_url, output_file, input_post_id, output_folder, output
         print(f"Error while downloading and converting .m3u8 to .mp4: {e}")
         return False
 
-session = requests.Session()
-config_file_path = 'config.ini'
+def main():
+    session = requests.Session()
+    config_file_path = 'config.ini'
 
-if os.path.isfile(config_file_path):
-    config = configparser.ConfigParser()
-    config.read(config_file_path)
-    output_dir = config.get('Settings', 'output_dir')
-else:
-    output_dir = input("Enter the output directory: ")
-    config = configparser.ConfigParser()
-    config['Settings'] = {'output_dir': output_dir}
-    with open(config_file_path, 'w') as configfile:
-        config.write(configfile)
-
-input_option = input("Choose an option (1 = .txt file, 2 = post id): ").strip().lower()
-if input_option == '1':
-    txt_files = list_txt_files(output_dir)
-    if not txt_files:
-        print("No .txt files found in the output directory.")
-        sys.exit(1)
-    chosen_file = choose_file_from_list(txt_files)
-    input_file_path = os.path.join(output_dir, chosen_file)
-    with open(input_file_path, 'r') as id_file:
-        post_ids = [line.strip() for line in id_file.readlines()]
-
-    selected_resolution = 'fhd'
-    original_resolution = selected_resolution
-
-    def download_videos_concurrently(session, post_ids, selected_resolution, output_dir):
-        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-            futures = []
-            for input_post_id in post_ids:
-                url = f"https://api.myfans.jp/api/v2/posts/{input_post_id}"
-                headers = read_headers_from_file("header.txt")
-                response = session.get(url, headers=headers)
-                if response.status_code == 200:
-                    data = response.json()
-                    main_videos = data['videos']['main']
-                    name_creator = data['user']['username']
-                    
-                    if main_videos:
-                        fhd_video = None
-                        sd_video = None
-                        for video in main_videos:
-                            if video["resolution"] == 'fhd':
-                                fhd_video = video
-                            elif video["resolution"] == 'sd':
-                                sd_video = video
-
-                        if fhd_video:
-                            selected_resolution = 'fhd'
-                            selected_video = fhd_video
-                        elif sd_video:
-                            selected_resolution = 'sd'
-                            selected_video = sd_video
-
-                        video_url = selected_video["url"]
-                        video_base_url, video_extension = os.path.splitext(video_url)
-                        if selected_resolution == "fhd":
-                            target_resolution = "1080p"
-                        elif selected_resolution == "sd":
-                            target_resolution = "480p"
-
-                        m3u8_url = f"{video_base_url}/{target_resolution}.m3u8"
-                        m3u8_response = session.get(m3u8_url, headers=headers)
-                        
-                        if video_url and m3u8_response.status_code == 200 and target_resolution == "1080p":
-                            m3u8_url = f"{video_base_url}/1080p.m3u8"
-                            output_folder = os.path.join(output_dir, name_creator)
-                            mp4_output_file = os.path.join(output_folder, f"{input_post_id}.mp4")
-                            print(f"This video is 1080p")
-                            future = executor.submit(DL_File, session, m3u8_url, mp4_output_file, input_post_id, output_folder, f"{input_post_id}.mp4")
-                            futures.append(future)
-                            
-                        elif video_url and m3u8_response.status_code == 200 and target_resolution == "480p":
-                            m3u8_url = f"{video_base_url}/480p.m3u8"
-                            output_folder = os.path.join(output_dir, name_creator)
-                            mp4_output_file = os.path.join(output_folder, f"{input_post_id}.mp4")
-                            print(f"This video is 480p")
-                            future = executor.submit(DL_File, session, m3u8_url, mp4_output_file, input_post_id, output_folder, f"{input_post_id}.mp4")
-                            futures.append(future)
-                            
-                        else:
-                            m3u8_url = f"{video_base_url}/360p.m3u8"
-                            output_folder = os.path.join(output_dir, name_creator)
-                            mp4_output_file = os.path.join(output_folder, f"{input_post_id}.mp4")
-                            print(f"This video is 360p")
-                            future = executor.submit(DL_File, session, m3u8_url, mp4_output_file, input_post_id, output_folder, f"{input_post_id}.mp4")
-                            futures.append(future)
-                                
-                    else:
-                        print(f"No videos found or You don't have access to this file for post ID {input_post_id}.")
-                else:
-                    print(f"Request failed for post ID {input_post_id} with status code:", response.status_code)
-            
-            concurrent.futures.wait(futures)
-    download_videos_concurrently(session, post_ids, selected_resolution, output_dir)
-
-elif input_option == '2':    
-    input_post_id = input("Enter the post ID: ")
-    post_ids = [input_post_id]
-    selected_resolution = 'fhd'
-    original_resolution = selected_resolution
-
-    for input_post_id in post_ids:
-        url = f"https://api.myfans.jp/api/v2/posts/{input_post_id}"
-        headers = read_headers_from_file("header.txt")
-        response = session.get(url, headers=headers)
-
-        if response.status_code == 200:
-            data = response.json()
-            main_videos = data['videos']['main']
-            name_creator = data['user']['username']
-
-            if main_videos:
-                fhd_video = None
-                sd_video = None
-                for video in main_videos:
-                    if video["resolution"] == 'fhd':
-                        fhd_video = video
-                    elif video["resolution"] == 'sd':
-                        sd_video = video
-                        
-                if fhd_video:
-                    selected_resolution = 'fhd'
-                    selected_video = fhd_video
-                elif sd_video:
-                    selected_resolution = 'sd'
-                    selected_video = sd_video
-                    
-                video_url = selected_video["url"]
-                video_base_url, video_extension = os.path.splitext(video_url)
-                if selected_resolution == "fhd":
-                    target_resolution = "1080p"
-                elif selected_resolution == "sd":
-                    target_resolution = "480p"
-
-                m3u8_url = f"{video_base_url}/{target_resolution}.m3u8"
-                m3u8_response = session.get(m3u8_url, headers=headers)
-                output_folder = os.path.join(output_dir, name_creator)
-
-                if video_url and m3u8_response.status_code == 200 and target_resolution == "1080p":
-                    m3u8_url = f"{video_base_url}/1080p.m3u8"
-                    mp4_output_file = os.path.join(output_folder, f"{input_post_id}.mp4")
-                    print(f"This video is 1080p")
-                    if DL_File(session, m3u8_url, mp4_output_file, input_post_id, output_folder, f"{input_post_id}.mp4"):
-                        pass
-                        
-                elif video_url and m3u8_response.status_code == 200 and target_resolution == "480p":
-                    m3u8_url = f"{video_base_url}/480p.m3u8"
-                    mp4_output_file = os.path.join(output_folder, f"{input_post_id}.mp4")
-                    print(f"This video is 480p")
-                    if DL_File(session, m3u8_url, mp4_output_file, input_post_id, output_folder, f"{input_post_id}.mp4"):
-                        pass
-                        
-                else:
-                    m3u8_url = f"{video_base_url}/360p.m3u8"
-                    mp4_output_file = os.path.join(output_folder, f"{input_post_id}.mp4")
-                    print(f"This video is 360p")
-                    if DL_File(session, m3u8_url, mp4_output_file, input_post_id, output_folder, f"{input_post_id}.mp4"):
-                        pass
-
-            else:
-                print(f"No '{selected_resolution}' videos found in the API response for post ID {input_post_id}.")
+    while True:
+        if os.path.isfile(config_file_path):
+            config = configparser.ConfigParser()
+            config.read(config_file_path)
+            output_dir = config.get('Settings', 'output_dir')
         else:
-            print(f"Request failed for post ID {input_post_id} with status code:", response.status_code)
-else:
-    print("Invalid option. Please choose 1 or 2.")
+            output_dir = input("Enter the output directory: ")
+            if output_dir == '0':
+                continue
+            config = configparser.ConfigParser()
+            config['Settings'] = {'output_dir': output_dir}
+            with open(config_file_path, 'w') as configfile:
+                config.write(configfile)
+
+        while True:
+            input_option = input("Choose an option (1 = .txt file, 2 = post id, 0 = go back): ").strip().lower()
+            if input_option == '0':
+                subprocess.run(["python", "./main.py"])
+            elif input_option == '1':
+                txt_files = list_txt_files(output_dir)
+                if not txt_files:
+                    print("No .txt files found in the output directory.")
+                    continue
+                chosen_file = choose_file_from_list(txt_files)
+                if chosen_file is None:
+                    continue
+                input_file_path = os.path.join(output_dir, chosen_file)
+                with open(input_file_path, 'r') as id_file:
+                    post_ids = [line.strip() for line in id_file.readlines()]
+
+                selected_resolution = 'fhd'
+                original_resolution = selected_resolution
+
+                download_videos_concurrently(session, post_ids, selected_resolution, output_dir)
+
+            elif input_option == '2':
+                input_post_id = input("Enter the post ID (or type '0' to go back): ")
+                if input_post_id == '0':
+                    continue
+                post_ids = [input_post_id]
+                selected_resolution = 'fhd'
+                original_resolution = selected_resolution
+
+                for input_post_id in post_ids:
+                    url = f"https://api.myfans.jp/api/v2/posts/{input_post_id}"
+                    headers = read_headers_from_file("header.txt")
+                    response = session.get(url, headers=headers)
+
+                    if response.status_code == 200:
+                        data = response.json()
+                        main_videos = data['videos']['main']
+                        name_creator = data['user']['username']
+
+                        if main_videos:
+                            fhd_video = None
+                            sd_video = None
+                            for video in main_videos:
+                                if video["resolution"] == 'fhd':
+                                    fhd_video = video
+                                elif video["resolution"] == 'sd':
+                                    sd_video = video
+                                    
+                            if fhd_video:
+                                selected_resolution = 'fhd'
+                                selected_video = fhd_video
+                            elif sd_video:
+                                selected_resolution = 'sd'
+                                selected_video = sd_video
+                                
+                            video_url = selected_video["url"]
+                            video_base_url, video_extension = os.path.splitext(video_url)
+                            if selected_resolution == "fhd":
+                                target_resolution = "1080p"
+                            elif selected_resolution == "sd":
+                                target_resolution = "480p"
+
+                            m3u8_url = f"{video_base_url}/{target_resolution}.m3u8"
+                            m3u8_response = session.get(m3u8_url, headers=headers)
+                            output_folder = os.path.join(output_dir, name_creator)
+
+                            if video_url and m3u8_response.status_code == 200 and target_resolution == "1080p":
+                                m3u8_url = f"{video_base_url}/1080p.m3u8"
+                                mp4_output_file = os.path.join(output_folder, f"{input_post_id}.mp4")
+                                print(f"This video is 1080p")
+                                if DL_File(session, m3u8_url, mp4_output_file, input_post_id, output_folder, f"{input_post_id}.mp4"):
+                                    pass
+                                    
+                            elif video_url and m3u8_response.status_code == 200 and target_resolution == "480p":
+                                m3u8_url = f"{video_base_url}/480p.m3u8"
+                                mp4_output_file = os.path.join(output_folder, f"{input_post_id}.mp4")
+                                print(f"This video is 480p")
+                                if DL_File(session, m3u8_url, mp4_output_file, input_post_id, output_folder, f"{input_post_id}.mp4"):
+                                    pass
+                                    
+                            else:
+                                m3u8_url = f"{video_base_url}/360p.m3u8"
+                                mp4_output_file = os.path.join(output_folder, f"{input_post_id}.mp4")
+                                print(f"This video is 360p")
+                                if DL_File(session, m3u8_url, mp4_output_file, input_post_id, output_folder, f"{input_post_id}.mp4"):
+                                    pass
+
+                        else:
+                            print(f"No '{selected_resolution}' videos found in the API response for post ID {input_post_id}.")
+                    else:
+                        print(f"Request failed for post ID {input_post_id} with status code:", response.status_code)
+            else:
+                print("Invalid option. Please choose 1 or 2.")
+
+def download_videos_concurrently(session, post_ids, selected_resolution, output_dir):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        futures = []
+        for input_post_id in post_ids:
+            url = f"https://api.myfans.jp/api/v2/posts/{input_post_id}"
+            headers = read_headers_from_file("header.txt")
+            response = session.get(url, headers=headers)
+            if response.status_code == 200:
+                data = response.json()
+                main_videos = data['videos']['main']
+                name_creator = data['user']['username']
+                
+                if main_videos:
+                    fhd_video = None
+                    sd_video = None
+                    for video in main_videos:
+                        if video["resolution"] == 'fhd':
+                            fhd_video = video
+                        elif video["resolution"] == 'sd':
+                            sd_video = video
+
+                    if fhd_video:
+                        selected_resolution = 'fhd'
+                        selected_video = fhd_video
+                    elif sd_video:
+                        selected_resolution = 'sd'
+                        selected_video = sd_video
+
+                    video_url = selected_video["url"]
+                    video_base_url, video_extension = os.path.splitext(video_url)
+                    if selected_resolution == "fhd":
+                        target_resolution = "1080p"
+                    elif selected_resolution == "sd":
+                        target_resolution = "480p"
+
+                    m3u8_url = f"{video_base_url}/{target_resolution}.m3u8"
+                    m3u8_response = session.get(m3u8_url, headers=headers)
+                    
+                    if video_url and m3u8_response.status_code == 200 and target_resolution == "1080p":
+                        m3u8_url = f"{video_base_url}/1080p.m3u8"
+                        output_folder = os.path.join(output_dir, name_creator)
+                        mp4_output_file = os.path.join(output_folder, f"{input_post_id}.mp4")
+                        print(f"This video is 1080p")
+                        future = executor.submit(DL_File, session, m3u8_url, mp4_output_file, input_post_id, output_folder, f"{input_post_id}.mp4")
+                        futures.append(future)
+                        
+                    elif video_url and m3u8_response.status_code == 200 and target_resolution == "480p":
+                        m3u8_url = f"{video_base_url}/480p.m3u8"
+                        output_folder = os.path.join(output_dir, name_creator)
+                        mp4_output_file = os.path.join(output_folder, f"{input_post_id}.mp4")
+                        print(f"This video is 480p")
+                        future = executor.submit(DL_File, session, m3u8_url, mp4_output_file, input_post_id, output_folder, f"{input_post_id}.mp4")
+                        futures.append(future)
+                        
+                    else:
+                        m3u8_url = f"{video_base_url}/360p.m3u8"
+                        output_folder = os.path.join(output_dir, name_creator)
+                        mp4_output_file = os.path.join(output_folder, f"{input_post_id}.mp4")
+                        print(f"This video is 360p")
+                        future = executor.submit(DL_File, session, m3u8_url, mp4_output_file, input_post_id, output_folder, f"{input_post_id}.mp4")
+                        futures.append(future)
+                            
+                else:
+                    print(f"No videos found or You don't have access to this file for post ID {input_post_id}.")
+            else:
+                print(f"Request failed for post ID {input_post_id} with status code:", response.status_code)
+        
+        concurrent.futures.wait(futures)
+
+if __name__ == "__main__":
+    main()
--- scripts/myfans_scrap.py
+++ scripts/myfans_scrap.py
@@ -2,6 +2,7 @@ import requests
 import json
 import os
 import configparser
+import subprocess
 
 def read_headers_from_file(filename):
     headers = {}
@@ -14,35 +15,59 @@ def read_headers_from_file(filename):
 def get_posts_for_page(base_url, page, headers):
     url = base_url + str(page)
     response = requests.get(url, headers=headers)
+    response.raise_for_status() 
     json_data = response.json()
     return json_data.get("data", [])
 
+def get_output_dir(config_file_path):
+    if os.path.isfile(config_file_path):
+        config = configparser.ConfigParser()
+        config.read(config_file_path)
+        return config.get('Settings', 'output_dir')
+    else:
+        output_dir = input("Enter the output directory: ")
+        config = configparser.ConfigParser()
+        config['Settings'] = {'output_dir': output_dir}
+        with open(config_file_path, 'w') as configfile:
+            config.write(configfile)
+        return output_dir
+
+def export_ids_to_file(posts, filename, save_free_only):
+    if os.path.isfile(filename):
+        base, ext = os.path.splitext(filename)
+        count = 1
+        while os.path.isfile(filename):
+            filename = os.path.join(output_dir, f"{base}_{count}{ext}")
+            count += 1
+    with open(filename, "w") as file:
+        for post in posts:
+            if save_free_only and not post.get("free"):
+                continue
+            file.write(f"{post.get('id')}\n")
+    print(f"Data exported to {filename}")
+
 config_file_path = 'config.ini'
+output_dir = get_output_dir(config_file_path)
 
-if os.path.isfile(config_file_path):
-    config = configparser.ConfigParser()
-    config.read(config_file_path)
-    output_dir = config.get('Settings', 'output_dir')
-else:
-    output_dir = input("Enter the output directory: ")
-    config = configparser.ConfigParser()
-    config['Settings'] = {'output_dir': output_dir}
-    with open(config_file_path, 'w') as configfile:
-        config.write(configfile)
+while True:
+    name_creator = input("Enter a name creator (no require @) or type '0' to go back: ")
+    if name_creator.lower() == '0':
+        subprocess.run(["python", "./main.py"])
+        exit()
+    new_base_url = f"https://api.myfans.jp/api/v2/users/show_by_username?username={name_creator}"
+    headers = read_headers_from_file("header.txt")
+    response = requests.get(new_base_url, headers=headers)
+    if response.status_code == 200:
+        new_json_data = response.json()
+        user_id = new_json_data.get("id")
+        if user_id:
+            break
+        else:
+            print("Failed to retrieve user id from the new API endpoint. Please try again.")
+    else:
+        print("Failed to connect to the API. Please check the username and try again.")
 
-name_creator = input("Enter a name creator (no require @): ")
-new_base_url = f"https://api.myfans.jp/api/v2/users/show_by_username?username={name_creator}"
-headers = read_headers_from_file("header.txt")
-response = requests.get(new_base_url, headers=headers)
-new_json_data = response.json()
-user_id = new_json_data.get("id")
-
-page = 1
-if user_id:
-    base_url = f"https://api.myfans.jp/api/v2/users/{user_id}/posts?sort_key=publish_start_at&page="
-else:
-    print("Failed to retrieve user id from the new API endpoint.")
-    exit()
+base_url = f"https://api.myfans.jp/api/v2/users/{user_id}/posts?sort_key=publish_start_at&page="
 
 print("Choose the type of posts to display:")
 print("1. Video")
@@ -50,26 +75,21 @@ print("2. Image")
 print("3. Show All")
 choice = input("Enter your choice (1/2/3): ")
 
-video_count = 0
-image_count = 0
 video_posts = []
 image_posts = []
-all_posts = []
 
+page = 1
 while True:
     page_data = get_posts_for_page(base_url, page, headers)
-    
     if not page_data:
         break
     page += 1
 
     for post in page_data:
-        post_kind = post.get("kind")
-        if post_kind == "video":
+        if post.get("kind") == "video":
             video_posts.append(post)
-        elif post_kind == "image":
+        elif post.get("kind") == "image":
             image_posts.append(post)
-        all_posts.append(post)
 
     for post in page_data:
         post_id = post.get("id")
@@ -78,61 +98,29 @@ while True:
         is_free = post.get("free")
 
         if post_id and post_kind and humanized_publish_start_at:
-            if choice == "1" and post_kind == "video":
-                video_count += 1
-                print(f"ID: {post_id}, Publish Date: {humanized_publish_start_at}, Free: {'yes' if is_free else 'no'}")
-            elif choice == "2" and post_kind == "image":
-                image_count += 1
-                print(f"ID: {post_id}, Publish Date: {humanized_publish_start_at}, Free: {'yes' if is_free else 'no'}")
-            elif choice == "3":
+            if (choice == "1" and post_kind == "video") or \
+               (choice == "2" and post_kind == "image") or \
+               choice == "3":
                 print(f"ID: {post_id}, Type: {post_kind}, Publish Date: {humanized_publish_start_at}, Free: {'yes' if is_free else 'no'}")
-                if post_kind == "video":
-                    video_count += 1
-                elif post_kind == "image":
-                    image_count += 1
+
+video_count = len(video_posts)
+image_count = len(image_posts)
 
 if choice == "1":
     print(f"Total Video Count: {video_count}")
-if choice == "2":
+elif choice == "2":
     print(f"Total Image Count: {image_count}")
-if choice == "3":
+elif choice == "3":
     print(f"Total Video Count: {video_count}")
     print(f"Total Image Count: {image_count}")
 
 export_id_choice = input("Do you want to export 'ID'? (Yes|y/No|n): ").strip().lower()
-if export_id_choice == "yes" or export_id_choice == "y":
+if export_id_choice in ("yes", "y"):
     save_choice = input("Do you want to save only free posts? (Yes|y/No|n): ").strip().lower()
+    save_free_only = save_choice in ("yes", "y")
 
-    if (choice == "1" or choice == "3") and video_posts:
-        filename = os.path.join(output_dir, f"{name_creator}_video_id.txt")
-        if os.path.isfile(filename):
-            base, ext = os.path.splitext(filename)
-            count = 1
-            while os.path.isfile(filename):
-                filename = os.path.join(output_dir, f"{base}_{count}{ext}")
-                count += 1
-        with open(filename, "w") as video_posts_id_file:
-            for post in video_posts:
-                post_id = post.get("id")
-                if (save_choice == "yes" or save_choice == "y") and not post.get("free"):
-                    continue
-                video_posts_id_file.write(f"{post_id}\n")
-        print(f"Video data exported to {filename}")
+    if choice in ("1", "3") and video_posts:
+        export_ids_to_file(video_posts, os.path.join(output_dir, f"{name_creator}_video_id.txt"), save_free_only)
 
-    if (choice == "2" or choice == "3") and image_posts:
-        filename = os.path.join(output_dir, f"{name_creator}_image_id.txt")
-        if os.path.isfile(filename):
-            base, ext = os.path.splitext(filename)
-            count = 1
-            while os.path.isfile(filename):
-                filename = os.path.join(output_dir, f"{base}_{count}{ext}")
-                count += 1
-        with open(filename, "w") as image_posts_id_file:
-            for post in image_posts:
-                post_id = post.get("id")
-                if (save_choice == "yes" or save_choice == "y") and not post.get("free"):
-                    continue
-                image_posts_id_file.write(f"{post_id}\n")
-        print(f"Image data exported to {filename}")
-else:
-    exit()
+    if choice in ("2", "3") and image_posts:
+        export_ids_to_file(image_posts, os.path.join(output_dir, f"{name_creator}_image_id.txt"), save_free_only)
